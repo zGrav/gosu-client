2a998fa2316a816490832d07868331764e6072d5
David Silva <zgrav@go.su>
2016-12-21 23:00:50 +0000

    small fix

--- src/client.js
+++ src/client.js
@@ -421,7 +421,7 @@ function onReceiveChatMessage(wrapper) {
 			return null;
 		}
 
-		if (msg.body_annotations.length > 0) {
+		if (msg.body_annotations) {
 			msg.body_annotations.map(function(keys) {
 				if (keys.type !== 'USER_MENTION' || keys.type !== 'WEB_LINK') {
 					return null; //because bot isn't really supposed to reply to anything else except these... as of now.
@@ -429,10 +429,10 @@ function onReceiveChatMessage(wrapper) {
 			});
 		}
 
-		if (msg.attachments.length > 0) {
+		if (msg.attachments > 0) {
 			msg.attachments.map(function (keys) {
 				if (keys.type !== 'REPLY_TO') {
-					return null;
+					return null; //because bot isn't really supposed to reply to anything else except these... as of now.
 				}
 			});
 		}
